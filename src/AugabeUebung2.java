22fa32859cc46c205ce73b09e954828671d004b9
TheresaSchmotz <schmotz.theresa@gmail.com>
2018-11-28 23:58:33 +0100

    AufgabeUebung2 mit i+=2, kommentiert.

--- src/AugabeUebung2.java
+++ src/AugabeUebung2.java
@@ -1,7 +1,7 @@
 public class AugabeUebung2 {
     public static void main(String[] args)
     {
-        for (int i = 1; i<4; i++)
+        for (int i = 1; i<4; i+=2)
         {
             printName();
         }
@@ -13,3 +13,6 @@ public class AugabeUebung2 {
         }
 
 }
+
+/*Benutze ich i+=2 statt i++ wird mir der wert nur zweimal anstatt der erwünschten dreimal ausgegeben.
+  Mit i+=2 wird der wert erhöht bis er den wert 2 erreicht hat.*/
