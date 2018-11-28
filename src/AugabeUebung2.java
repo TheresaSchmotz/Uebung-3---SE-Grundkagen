6c415a33463f804e89f2203923389101a51b777b
TheresaSchmotz <schmotz.theresa@gmail.com>
2018-11-28 23:53:53 +0100

    AufgabeUebung2 i++

---
+++ src/AugabeUebung2.java
@@ -0,0 +1,15 @@
+public class AugabeUebung2 {
+    public static void main(String[] args)
+    {
+        for (int i = 1; i<4; i++)
+        {
+            printName();
+        }
+    }
+        public static void printName()
+        {
+            String meinName = "Theresa";
+            System.out.println(meinName);
+        }
+
+}
