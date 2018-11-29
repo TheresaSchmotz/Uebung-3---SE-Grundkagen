ddf259ae390db723fd7d47750ed723e512f941bc
TheresaSchmotz <schmotz.theresa@gmail.com>
2018-11-29 01:28:08 +0100

    Aufgabe 5

---
+++ src/Rechnen.java
@@ -0,0 +1,17 @@
+public class Rechnen {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void add(int a, int b){
+        int add1 = a+b;
+        System.out.println(a+" "+b);
+    }
+
+    public static void add(double a, double b){
+        double double1 = a+b;
+        System.out.println(a+" "+b);
+    }
+
+}
