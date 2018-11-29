7ffbae356a8c616d17ea854a25cafb0770aa106f
TheresaSchmotz <schmotz.theresa@gmail.com>
2018-11-29 01:50:11 +0100

    vehicle kram

---
+++ src/Vehicle.java
@@ -0,0 +1,15 @@
+public class Vehicle {
+
+    static int doors;
+    static int ps;
+    static String color;
+
+    public static void tuerenzaehler(String[]tueren)
+    {
+        System.out.println("Das Fahrzeug besitzt"+doors+"Türen");
+    }
+
+    public static void autofarbe(String[]farbe)
+    {
+        System.out.println(color);
+    }
